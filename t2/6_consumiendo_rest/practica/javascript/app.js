48d167a26914cbe818c2c2b357b1269fc6f9ae46
brodriguezaybar <brodriguezaybar@gmail.com>
2015-07-10 20:02:39 -0500

    Subir clase 5

---
+++ t2/6_consumiendo_rest/practica/javascript/app.js
@@ -0,0 +1,20 @@
+angular.module("devflix", ["series", "serie", "contacto", "ngRoute"]);
+
+angular.module("devflix").config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "templates/series.html",
+      controller: "SeriesController"
+    })
+    .when("/contacto", {
+      templateUrl: "templates/contacto.html",
+      controller: "ContactoController"
+    })
+    .when("/series/:serie", {
+      templateUrl: "templates/serie.html",
+      controller: "SerieController",
+      controllerAs: "serieCtrl"
+    });
+
+  $locationProvider.html5Mode(true);
+});
---
+++ t2/6_consumiendo_rest/practica/javascript/series.js
@@ -0,0 +1,5 @@
+angular.module("series", []);
+
+angular.module("series").controller("SeriesController", function () {
+
+});
